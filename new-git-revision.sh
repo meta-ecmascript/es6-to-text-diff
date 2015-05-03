b120b3c004350ad099acb64569c3548f74af9ca0
azu <azuciao@gmail.com>
2015-05-04 02:52:03 +0900

    improve git script

--- new-git-revision.sh
+++ new-git-revision.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-declare txtFiles=$(ls txts | sort --field-separator=v -k 1,1n -k 2,2n -k 3,3n -k 4,4n)
+declare txtFiles=$(find "$(pwd)/txts" -maxdepth 1 -mindepth 1 | sort --field-separator=v -k 1,1n -k 2,2n -k 3,3n -k 4,4n)
 declare outputDir="es6-draft-revision"
 rm -rf ${outputDir}
 mkdir -p ${outputDir}
-git --separate-git-dir init ${outputDir}
+cd ${outputDir}
+git init
 for file in ${txtFiles} ; do
-  cp "${file}" "${outputDir}/es6-draft.txt"
-  git --git-dir "${outputDir}/.git" add "${outputDir}/es6-draft.txt"
-  commitMessage=$(basename "${file}" .doc.docx.txt)
-  git --git-dir "${outputDir}/.git" commit -m "${commitMessage}"
+  if [ -s "${file}" ]; then
+    cp "${file}" ./es6-draft.txt
+    # remove "_" from "_some_"
+    sed -i '' -e 's/_\([^ ]*\)_/\1/g' es6-draft.txt
+    git add ./es6-draft.txt
+    commitMessage=$(basename "${file}" .doc.docx.txt)
+    git commit -m "${commitMessage}"
+  fi
 done
+cd -
