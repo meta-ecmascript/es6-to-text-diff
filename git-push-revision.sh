316c9bfa043342f0d16003c2982989bd7055891a
azu <azuciao@gmail.com>
2015-05-04 14:24:29 +0900

    add guard

--- git-push-revision.sh
+++ git-push-revision.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd es6-draft-revision/
+declare currentDirName=${PWD##*/}
+if [ "${currentDirName}" != "es6-draft-revision" ]; then
+    echo "${currentDirName} is not es6-draft-revision"
+    exit 1;
+fi
 # git push
 git remote add origin git@github.com:meta-ecmascript/es6-draft-revision.git
 git push --set-upstream origin master -f --follow-tags
